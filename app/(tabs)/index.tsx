bb28bb2d76d650e6689b519d3eb6cf0693cb60c0
szymonrybczak <szymon.rybczak@gmail.com>
2025-06-06 18:14:48 +0200

    unistyles crash

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,81 +1,23 @@
-import { Button, SafeAreaView, Text, View } from "react-native";
-
-import SegmentedControl from "@react-native-segmented-control/segmented-control";
-import { useState } from "react";
-import { ScopedTheme, StyleSheet } from "react-native-unistyles";
+import { ScrollView, Text, View } from "react-native";
+import { StyleSheet } from "react-native-unistyles";
 
 function FirstScreen() {
   return (
-    <View style={styles.firstScreen}>
-      <ScopedTheme name="other">
-        <Button title="Say hello" onPress={() => {}} />
-        <Text style={styles.firstScreenText}>First Screen</Text>
-      </ScopedTheme>
-    </View>
+    <ScrollView style={{ paddingRight: 16 }}>
+      <View style={styles.container}>
+        <Text>Hello</Text>
+      </View>
+    </ScrollView>
   );
 }
 
-function SecondScreen() {
-  return (
-    <View style={styles.secondScreen}>
-      <ScopedTheme name="light">
-        <Text style={styles.secondScreenText}>Second Screen</Text>
-      </ScopedTheme>
-    </View>
-  );
-}
-
-export default function HomeScreen() {
-  const [tab, setTab] = useState("a");
-  return (
-    <SafeAreaView style={styles.container}>
-      <SegmentedControl
-        onValueChange={(value) => {
-          setTab(value);
-        }}
-        values={["a", "b"]}
-        selectedIndex={tab === "a" ? 0 : 1}
-      />
-      {tab === "a" && <FirstScreen />}
-      {tab === "b" && <SecondScreen />}
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create((theme) => ({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "red",
   },
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
-  },
-  firstScreen: {
-    flex: 1,
-    justifyContent: "flex-end",
-    paddingBottom: 100,
-  },
-  firstScreenText: {
-    color: theme.colors.secondary,
-  },
-  secondScreen: {
-    flex: 1,
-    justifyContent: "flex-end",
-    paddingBottom: 100,
-  },
-  secondScreenText: {
-    color: theme.colors.secondary,
-  },
-}));
+});
+
+export default function HomeScreen() {
+  return <FirstScreen />;
+}
